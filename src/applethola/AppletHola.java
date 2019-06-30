9d198a1f4ec203b0510389a67fd82b163901f3da
Camilo Ceron <camiloceron1990@gmail.com>
2019-06-30 09:44:41 -0500

    Hola Java

---
+++ src/applethola/AppletHola.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applethola;
+
+/**
+ *
+ * @author camilo
+ */
+import java.awt.*; 
+import java.applet.*; 
+public class AppletHola extends Applet{ 
+    public void paint(Graphics g) { 
+        g.drawString("Hola", 100, 100); 
+    } 
+} 
